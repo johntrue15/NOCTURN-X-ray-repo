b5ef3d486335271bb4fd42175835cb882dfda758
johntrue15 <65981672+johntrue15@users.noreply.github.com>
2025-01-02 21:03:43 -0500

    Update move_slices_and_screenshots.py

--- .github/scripts/move_slices_and_screenshots.py
+++ .github/scripts/move_slices_and_screenshots.py
@@ -20,7 +20,7 @@ def move_slices_and_screenshots():
     options = webdriver.ChromeOptions()
 
     # >>> Uncomment this for GitHub Actions (headless) <<<
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
