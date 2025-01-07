b2f3f93c803393c7112c08b7f0e52cd48bc9f582
johntrue15 <65981672+johntrue15@users.noreply.github.com>
2025-01-06 21:29:50 -0500

    Update selenium_screenshot_new.py
    480 sleep

--- .github/scripts/selenium_screenshot_new.py
+++ .github/scripts/selenium_screenshot_new.py
@@ -118,7 +118,7 @@ def take_screenshot(url):
         print(f"\nProcessing ID {file_id}")
         print(f"Loading URL: {url}")
         driver.get(url)
-        time.sleep(120)
+        time.sleep(480)
 
         # Wait for iframe and switch
         wait = WebDriverWait(driver, 10)
